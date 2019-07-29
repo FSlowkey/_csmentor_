aee760fdbe9094373941107a14596abc10203845
steph-osoriot <steph.osoriot@gmail.com>
2019-07-29 11:06:51 -0400

    updated main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def render_template(handler, file_name, template_values):
 
 def get_user_email():
     user = users.get_current_user()
-    if user:  # this means that it's checking if the object user exists, exists = true
-        return user.email()
-    else:
-        return None
+    #if user:  # this means that it's checking if the object user exists, exists = true
+     #   return user.email()
+    #else:
+     #   return None
 
 
 def get_template_parameters():
@@ -56,37 +56,16 @@ class ProfileEditHandler(webapp2.RequestHandler):
 
             
 class ProfileSaveHandler(webapp2.RequestHandler):
-
     def post(self):
         email = get_user_email()
-        if not email:
-            self.redirect('/')
-        else:
-            error_text = ''
-            name = self.request.get('name')
-            description = self.request.get('description')
-            if len(name) < 2:
-                error_text += "name should be at least two characters.\n" 
-            if len(name) > 20:
-                error_text += 'name should be no more than 20 characters. \n'
-            if len(name.split()) > 1:
-                error_text += 'name should not have white spaces \n'
-            if len(description) > 4000:
-                error_text += 'description should be less than 4000 characters.\n'
-                        for word in description.split():
-                if len(word) > 50:
-                    error_text += 'description contains words that are too long.\n'
-                    break
-            values = get_template_parameters()
-            values['name'] = name
-            values['description'] = description
-
-            if error_text:
-                values['errormsg'] = error_text
-            else:
-                socialdata.save_profile(email, name, description)
-                values['successmsg'] = 'everything worked out fine'
-            render_template(self, 'profile-edit.html', values)
+        values = []
+        values['name'] = name
+        values['biography'] = biography
+        values['pic'] = pic
+        values['location'] = location
+        socialdata.save_profile(email, name, biography, pic, location)
+        
+    render_template(self, 'edit-profile.html',values)
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -97,4 +76,5 @@ class MainHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('.*', MainHandler),
+    ('/edit-profile, edit
 ])
