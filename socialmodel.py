1f3ed14c26d22caf618258047c219ce5c2ccce69
Minna-Gao <minna.gao.1017@gmail.com>
2019-07-29 18:00:17 -0400

    updates

--- socialmodel.py
+++ socialmodel.py
@@ -2,10 +2,10 @@ from google.appengine.ext import ndb
 
 # THIS IS THE FILE THAT DEFINES THE USER OBJECT
 
+
 class UserProfile(ndb.Model):
     name = ndb.StringProperty()
-    email= ndb.StringProperty()
+    email = ndb.StringProperty()
     biography= ndb.TextProperty()
     location= ndb.StringProperty()
-    #interests= ndb.PickleProperty()
-    
+    interests= ndb.PickleProperty()
